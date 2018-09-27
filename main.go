66afa9668e4de3f88071c831bb96741b32fb9640
admin22 <s.gavrilenko@lucky-labs.com>
2018-09-28 00:19:38 +0300

    prettify rendering template

--- main.go
+++ main.go
@@ -31,6 +31,10 @@ type task struct {
 	a        int
 	b        int
 	c        int
+	iter     int
+	level    int
+	result   interface{}
+	mission  string
 }
 
 var taskInfo task
@@ -42,6 +46,8 @@ func init() {
 	//flag.StringVar(&mongoDSN.db,"mongo.db","kidscalc","MongoDB db name")
 	//flag.StringVar(&mongoDSN.rs,"mongo.rs","rs0","MongoDB replica set")
 	flag.StringVar(&taskInfo.template, "task.template", "", "Task template. Example a+m*(b-c)")
+	flag.IntVar(&taskInfo.level, "task.level", 1, "Task level")
+	flag.IntVar(&taskInfo.iter, "task.iter", 0, "Start task iteration number from")
 	flag.IntVar(&taskInfo.factor, "task.m", 1, "m - multiplier in template")
 	flag.IntVar(&taskInfo.divider, "task.d", 1, "d - divider in template")
 	flag.IntVar(&taskInfo.a, "task.a", 1, "parameter a")
@@ -52,13 +58,26 @@ func init() {
 
 func renderTemplate(params map[string]interface{}) string {
 	var r *strings.Replacer
-
-	r = strings.NewReplacer("a", strconv.Itoa(params["a"].(int)),
-		"b", strconv.Itoa(params["b"].(int)),
-		"c", strconv.Itoa(params["c"].(int)),
-		"m", strconv.Itoa(params["m"].(int)),
-		"d", strconv.Itoa(params["d"].(int)))
-
+	switch {
+	case (params["m"] == 1) || (params["d"] == 1):
+		r = strings.NewReplacer(
+			"a", strconv.Itoa(params["a"].(int)),
+			"b", strconv.Itoa(params["b"].(int)),
+			"c", strconv.Itoa(params["c"].(int)),
+			"m*", "",
+			"*m", "",
+			"/d", "",
+			"d/", "",
+		)
+	default:
+		r = strings.NewReplacer(
+			"a", strconv.Itoa(params["a"].(int)),
+			"b", strconv.Itoa(params["b"].(int)),
+			"c", strconv.Itoa(params["c"].(int)),
+			"m", strconv.Itoa(params["m"].(int)),
+			"d", strconv.Itoa(params["d"].(int)),
+		)
+	}
 	return r.Replace(taskInfo.template)
 }
 
@@ -69,7 +88,7 @@ func main() {
 		log.Printf("Error: %s", err)
 	}
 
-	parameters["iter"] = 0
+	parameters["iter"] = taskInfo.iter
 	for d := 1; d <= taskInfo.divider; d++ {
 		parameters["d"] = d
 		for m := 1; m <= taskInfo.factor; m++ {
@@ -84,8 +103,10 @@ func main() {
 						if err != nil {
 							log.Printf("Error: %s", err)
 						}
+						taskInfo.result = result
+						taskInfo.mission = renderTemplate(parameters)
 						parameters["iter"] = parameters["iter"].(int) + 1
-						fmt.Printf("%d\t%s=%v\n", parameters["iter"], renderTemplate(parameters), result)
+						fmt.Printf("%d\t%s=%v\n", parameters["iter"], taskInfo.mission, result)
 					}
 				}
 			}
